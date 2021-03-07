403ee308a116151079b027cfda93b3c2aa893f03
Tino-Jokomo <tinojokomo@Tinos-MacBook-Air.local>
2021-03-07 18:34:32 +0200

    Fix commit 4

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const errorLink = onError(({graphqlErrors,networkError}) => {
 const link= from([
     errorLink,
     new HttpLink({
-        uri: "http://192.168.1.55:9000/graphql"
+        uri: "http://localhost:9000/graphql"
     })
 ])
 
@@ -33,7 +33,7 @@ const client =new ApolloClient ({
     link:link
 })
 
-
+//192.168.1.55
 
 
     
